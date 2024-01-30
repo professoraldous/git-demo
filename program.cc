de5c0f75ee692b1c0700114c44b640bf65571f15
Peter Aldous <Peter.Aldous@uvu.edu>
2024-01-30 13:46:01 -0700

    added a clarifying comment

--- program.cc
+++ program.cc
@@ -2,15 +2,12 @@
 
 using namespace std;
 
-void parser() {
-    cout << "STRUCTURE" << endl;
-}
+// This is a silly comment
+void parser() { cout << "STRUCTURE" << endl; }
 
-void lexer() {
-    cout << "Lexing correctly this time" << endl;
-}
+void lexer() { cout << "Lexing correctly this time" << endl; }
 
 int main() {
-    lexer();
-    cout << "We're doing a thing" << endl;
+  lexer();
+  cout << "We're doing a thing" << endl;
 }
